54a6c11b79aeed137cf69b1976c349cb58414e33
Taylor Barnett <taylor@transposit.com>
2019-11-06 19:01:59 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -5,27 +5,24 @@
   const userId = parsed_body.user_id;
   const response_url = parsed_body.response_url;
   
-  console.log(response_url);
-
   setImmediate(() => {
     let user = api.user({type: "slack", workspaceId, userId});
     if (user) {
-      console.log(user);
       var test = api.run("slack_webhook.post_message_to_incoming_webhook", {response_url : response_url});
       console.log(test);
-      var text_match = /(\S+) (\S+)/.exec(parsed_body.text.trim());
-      if (text_match) {
-        console.log(text_match);
+      // var text_match = /(\S+) (\S+)/.exec(parsed_body.text.trim());
+      // if (text_match) {
+        // console.log(text_match);
 		// let message = api.run('this.transfer_file', { source_url: text_match[1], target_url: text_match[2] }, {asUser: user.id})[0];
 		// api.run("slack_webhook.post_to_response_url", {
 		// response_url: response_url,
 		// post_body: {text: message}
 		// });      
-      } else {
-        api.run("slack_webhook.post_to_response_url", {
-          response_url: response_url,
-          post_body: {text: "Couldn't parse the source and target urls."}
-        });      
+      // } else {
+      //   api.run("slack_webhook.post_to_response_url", {
+      //     response_url: response_url,
+      //     post_body: {text: "Couldn't parse the source and target urls."}
+      //   });      
       }
     } else {
       api.run("slack_webhook.post_to_response_url", {
