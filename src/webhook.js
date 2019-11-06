ade482c59980c2e6f03d3af534012e763049d5cb
Taylor Barnett <taylor@transposit.com>
2019-11-06 19:19:27 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,7 +9,7 @@
     let user = api.user({type: "slack", workspaceId, userId});
     if (user) {
       var initial_response = api.run("this.post_message_to_incoming_webhook", {webhook_url: response_url});
-      console.log(initial_response);
+      // console.log(initial_response);
     } else {
       // api.run("slack_webhook.post_to_response_url", {
       //   response_url: response_url,
