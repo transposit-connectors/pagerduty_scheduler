156ff483132b980ea8f4e31ab92988046656b8a4
Taylor Barnett <taylor@transposit.com>
2019-11-06 19:58:59 +0000

    Commit from Transposit IDE

--- src/post_msg.js
+++ src/post_msg.js
@@ -1,7 +1,12 @@
-  const parameters = {};
-  parameters.webhook_url = params.webhook_url;
-  parameters.text = 'helloooo world';
-  //parameters.blocks = [];
-  //parameters.thread_ts = '<string>';
-  //parameters.response_type = '<string>';
-  return api.run('slack_webhook.post_message_to_incoming_webhook', parameters);
+(params) => {
+
+  webhook_url = params.webhook_url;
+  text = "hello world";
+  
+  return api.run("slack_webhook.post_message_to_incoming_webhook", webhook_url, text);
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
