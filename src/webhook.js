3ffbd73058e0a93bb2168ee65123a1aa4c654a26
Taylor Barnett <taylor@transposit.com>
2019-11-07 23:23:04 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -13,14 +13,14 @@
               stash.put("start_date",action_payload.actions[0].selected_date);
       		  api.run("this.respond_to_override_request_step_1",{http_event: http_event});
             } else if (action_payload.actions[0].action_id == "end_date") {
-              var end_date = action_payload.actions[0].selected_date;
+              stash.put("end_date",action_payload.actions[0].selected_date);
       		  api.run("this.respond_to_override_request_step_2",{http_event: http_event});              
             } else if (action_payload.actions[0].action_id == "start_time") {
-              var start_time = action_payload.actions[0].selected_option.text.text;
+              stash.put("start_time",action_payload.actions[0].selected_option.text.text);
       		  api.run("this.respond_to_override_request_step_3",{http_event: http_event});              
             } else if (action_payload.actions[0].action_id == "end_time") { 
-              var end_time = action_payload.actions[0].selected_option.text.text;
-      		  api.run("this.respond_to_override_request_step_4",{http_event: http_event, start_date: stash.get("start_date"), end_date: end_date, start_time: start_time, end_time: end_time});                            
+              stash.put("end_time",action_payload.actions[0].selected_option.text.text);
+      		  api.run("this.respond_to_override_request_step_4",{http_event: http_event, start_date: stash.get("start_date"), end_date: stash.get("end_date"), start_time: stash.get("start_time"), end_time: stash.get("end_date")});                            
             } else if (action_payload.actions[0].action_id == "override_request_submit") {
               console.log("COMPLETE!");
             }
