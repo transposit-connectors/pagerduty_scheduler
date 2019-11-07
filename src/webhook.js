ad58babded2737ad2a12d1630deb56562e734e42
Taylor Barnett <taylor@transposit.com>
2019-11-07 21:23:35 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -4,7 +4,7 @@
   const workspaceId = parsed_body.team_id;
   const userId = parsed_body.user_id;
   const response_url = parsed_body.response_url; 
-    
+  
   setImmediate(() => {
     if (parsed_body.payload) {
       const action_payload = JSON.parse(parsed_body.payload);
@@ -27,3 +27,28 @@
   return { status_code: 200 };
 
 }
+  
+  
+//   setImmediate(() => {
+//     if (parsed_body.payload) {
+//       const action_payload = JSON.parse(parsed_body.payload);
+//       if (action_payload.actions && action_payload.actions[0].value == "override_request_submission") {
+//         console.log("A submission has been receieved");
+//         var resp = api.run("this.respond_to_interaction", {http_event: http_event});
+//       }
+//     }
+//     let user = api.user({type: "slack", workspaceId, userId});
+//     if (user) {
+//       var command_response = api.run("this.respond_to_slash_command",{http_event: http_event});
+//     } else {
+//       // api.run("slack_webhook.post_to_response_url", {
+//       //   response_url: response_url,
+//       //   post_body: {text: 'Please configure your user at ' +  env.getBuiltin().appUrl}
+//       // });      
+//     }
+//   });
+//   //return api.run("slack_webhook.acknowledge_slash_command"); 
+//   return { status_code: 200 };
+
+// }
+  
