15c867c1683f28b39bdf9a88e9579f670ab53e14
Taylor Barnett <taylor@transposit.com>
2019-11-07 23:20:41 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -10,7 +10,7 @@
     	const action_payload = JSON.parse(parsed_body.payload);
     	if (action_payload.actions) {
             if (action_payload.actions[0].action_id == "start_date") {
-              var start_date = action_payload.actions[0].selected_date;
+              stash.put("start_date",action_payload.actions[0].selected_date);
       		  api.run("this.respond_to_override_request_step_1",{http_event: http_event});
             } else if (action_payload.actions[0].action_id == "end_date") {
               var end_date = action_payload.actions[0].selected_date;
@@ -20,7 +20,7 @@
       		  api.run("this.respond_to_override_request_step_3",{http_event: http_event});              
             } else if (action_payload.actions[0].action_id == "end_time") { 
               var end_time = action_payload.actions[0].selected_option.text.text;
-      		  api.run("this.respond_to_override_request_step_4",{http_event: http_event, start_date: start_date, end_date: end_date, start_time: start_time, end_time: end_time});                            
+      		  api.run("this.respond_to_override_request_step_4",{http_event: http_event, start_date: stash.get("start_date"), end_date: end_date, start_time: start_time, end_time: end_time});                            
             } else if (action_payload.actions[0].action_id == "override_request_submit") {
               console.log("COMPLETE!");
             }
