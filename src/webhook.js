4c4ee8a5f69fadccf985211abed965a1c01da7f6
Taylor Barnett <taylor@transposit.com>
2019-11-07 20:24:41 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -8,7 +8,7 @@
   setImmediate(() => {
     if (parsed_body.payload) {
       console.log(JSON.parse(parsed_body.payload));
-      if (parsed_body.payload.actions.[0].value == "override_request_submission") {
+      if (parsed_body.payload.actions[0].value == "override_request_submission") {
         var resp = api.run("this.respond_to_interaction", {http_event: http_event});
       }
     }
