a445c22087a004e833a19948f880aaf2bd4724c9
Taylor Barnett <taylor@transposit.com>
2019-11-11 20:19:03 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -26,7 +26,11 @@
               api.run("this.share_override_request", {http_event: http_event,start_date: stash.get("start_date"), end_date: stash.get("end_date"), start_time: stash.get("start_time"), end_time: stash.get("end_time")});
             } else if (action_payload.actions[0].action_id == "accept_override_request") {
               console.log("call pagerduty api");
-              api.run("this.post_schedules_by_id_overrides", start: stash.get("start_date") + " " + stash.get("start_time") + " UTC", end: stash.get("end_date") + " " + stash.get("end_time") + " UTC")
+              var start_date_time = stash.get("start_date") + " " + stash.get("start_time") + " UTC";
+              start_date_time = start_date_time.toISOString();
+              var end_date_time = stash.get("end_date") + " " + stash.get("end_time") + " UTC");
+              end_date_time = end_date_time.toISOString();
+              api.run("this.post_schedules_by_id_overrides", {start: start_date_time, end: end_date_time});
             }
     	}
     }
