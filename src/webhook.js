1e881f630c8c43e5ebfadd44e5fa3c75099df144
Taylor Barnett <taylor@transposit.com>
2019-11-11 19:45:22 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,7 +9,7 @@
     if (parsed_body.payload) {
     	const action_payload = JSON.parse(parsed_body.payload);
     	if (action_payload.actions) {
-            if (action_payload.actions[0].action_id == "start_date") {
+            if (action_payload.actions.action_id == "start_date") {
               stash.put("start_date",action_payload.actions[0].selected_date);
       		  api.run("this.respond_to_override_request_step_1",{http_event: http_event});
             } else if (action_payload.actions[0].action_id == "end_date") {
