30733a795f1c230b734fbbef52e33f415fa271cc
Taylor Barnett <taylor@transposit.com>
2019-11-12 22:56:08 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -13,7 +13,7 @@
       } 
       if (parsed_body.payload) {
     	const action_payload = JSON.parse(parsed_body.payload);
-        console.log(action_payload);
+        console.log("action payload" + action_payload);
     	if (action_payload.actions) {
             if (action_payload.actions[0].action_id == "start_date") {
               stash.put("start_date",action_payload.actions[0].selected_date);
