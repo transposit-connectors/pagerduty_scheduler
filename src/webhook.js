976e40d613f239e1c04d3e0335a283d6865e92cd
Taylor Barnett <taylor@transposit.com>
2019-11-12 22:48:28 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -8,9 +8,10 @@
   setImmediate(() => {  
     let user = api.user({type: "slack", workspaceId, userId});
     if (user) {
-      console.log(parsed_body.payload);
-      var command_response = api.run("this.respond_to_override_request_step_0",{http_event: http_event});
-	  if (parsed_body.payload) {
+      if (parsed_body.command == "/request-override") {
+      	var command_response = api.run("this.respond_to_override_request_step_0",{http_event: http_event});
+      }
+      if (parsed_body.payload) {
     	const action_payload = JSON.parse(parsed_body.payload);
     	if (action_payload.actions) {
             if (action_payload.actions[0].action_id == "start_date") {
