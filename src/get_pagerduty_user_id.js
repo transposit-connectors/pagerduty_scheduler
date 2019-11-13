1ed256d61eede94100441e9d4fd6e6a8179e1d56
Taylor Barnett <taylor@transposit.com>
2019-11-13 16:58:24 +0000

    Commit from Transposit IDE

---
+++ src/get_pagerduty_user_id.js
@@ -0,0 +1,13 @@
+(params) => {
+  
+  user_setting.get("pagerduty_user_id")
+  
+  return {
+    mission: "complete"
+  };
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
