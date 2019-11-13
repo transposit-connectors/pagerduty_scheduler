d68775e7d7ce148867ab00ac9530e58c185b4f94
Taylor Barnett <taylor@transposit.com>
2019-11-13 19:37:21 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -48,9 +48,9 @@
 				
 	  			var user = api.user({type: "slack", workspaceId: action_payload.team.id, userId: action_payload.user.id});
 				var pagerduty_user_id = api.run("this.get_pagerduty_user_id", {}, {"asUser":user.id})[0];                
-				var pageduty_override_response = api.run("this.post_schedules_by_id_overrides", {start: start_date_time.toISOString(), end: end_date_time.toISOString(), user_id: pagerduty_user_id});
+				var pageduty_override_response = api.run("this.post_schedules_by_id_overrides", {start: start_date_time.toISOString(), end: end_date_time.toISOString(), user_id: pagerduty_user_id})[0];
 				
-              	console.log(pageduty_override_response);
+              	console.log(pageduty_override_response.override.id);
 				if (pageduty_override_response) {
 				  api.run("this.confirm_override_scheduled", {http_event: http_event});
 				}
