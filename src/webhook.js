41aa990d4d61f9aef6d72bd876eb15c698b15372
Taylor Barnett <taylor@transposit.com>
2019-11-15 23:03:46 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -19,7 +19,6 @@
   });
 	 
   if (parsed_body.payload) {
-      console.log("111111");
 	  const action_payload = JSON.parse(parsed_body.payload);
 		var user = api.user({type: "slack", workspaceId: action_payload.team.id, userId: action_payload.user.id});                
 		if (user) {
@@ -52,9 +51,7 @@
 						api.run("this.confirm_override_scheduled", {http_event: http_event});
 					}
 				}
-				}
 	  } else {
-          console.log("22222");
 		  api.run("slack_webhook.respond_to_slash_command", {
 				http_event: http_event,
 				text: 'Please configure your user at ' +  env.getBuiltin().appUrl
