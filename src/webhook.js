5daf8108b2022eda52cea7ebeb18dd9dec3b3767
Taylor Barnett <taylor@transposit.com>
2019-11-18 17:21:34 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -45,7 +45,9 @@
 					var end_date_time = new Date(end_date_time_string);
 
 					var pagerduty_user_id = api.run("this.get_pagerduty_user_id", {}, {"asUser":user.id})[0];
-                  	console.log(start_date_time.toISOString());
+                  	
+                  console.log(state_date_time);
+                  console.log(start_date_time.toISOString());
                     // console.log(end_date_time.toISOString());
 					// var pageduty_override_response = api.run("this.post_schedules_by_id_overrides", {start: start_date_time.toISOString(), end: end_date_time.toISOString(), user_id: pagerduty_user_id})[0];
 
