04f8834b237e8eab821c83770e1a102eb7e34971
Taylor Barnett <taylor@transposit.com>
2019-11-18 17:27:41 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -41,14 +41,12 @@
 				} else if (action_payload.actions[0].action_id == "accept_override_request") {
 					var start_date_time_string = stash.get(user.id + "_start_date") + " " + stash.get(user.id + "_start_time") + " UTC";
 					var end_date_time_string = stash.get(user.id + "_end_date") + " " + stash.get(user.id + "_end_time") + " UTC";
-					var start_date_time = new Date(start_date_time_string);
+					console.log(start_date_time_string);
+                    var start_date_time = new Date(start_date_time_string);
 					var end_date_time = new Date(end_date_time_string);
 
 					var pagerduty_user_id = api.run("this.get_pagerduty_user_id", {}, {"asUser":user.id})[0];
                   	
-                  console.log(state_date_time);
-                  console.log(start_date_time.toISOString());
-                    // console.log(end_date_time.toISOString());
 					// var pageduty_override_response = api.run("this.post_schedules_by_id_overrides", {start: start_date_time.toISOString(), end: end_date_time.toISOString(), user_id: pagerduty_user_id})[0];
 
 					// console.log(pageduty_override_response);
